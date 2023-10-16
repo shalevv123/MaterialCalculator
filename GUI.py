df4cf9d6bd35cb26a3986e6e50b8d3ed37df8dc5
shalevv123 <shalev.riss@gmail.com>
2023-10-16 17:34:49 +0300

    alternative recepies

--- GUI.py
+++ GUI.py
@@ -1,4 +1,5 @@
 import customtkinter as tk
+
 from CTkMessagebox import CTkMessagebox
 import Material
 tk.set_appearance_mode("dark")
@@ -11,45 +12,62 @@ class GUI:
 
         def calculate_materials():
             data = {}
-            for entry, label in entry_label_pairs:
-                label_text = label.cget("text")
+            for entry, menu in entry_label_pairs:
+                menu_text = menu.get()
                 entry_text = entry.get()
                 if entry_text:
-                    data[label_text] = float(entry_text)
+                    data[menu_text] = float(entry_text)
             production_report = {}
             for name, amount in data.items():
-                Material.combineDict(production_report, Material.material_dict[name].produce(amount))
+                current_item, _ = Material.name_to_item_dict[name]
+                Material.combineDict(production_report,  current_item.produce(amount))
+
             if len(production_report) == 0:
-                CTkMessagebox(title="Error", message="Enter your required materials first")
+                CTkMessagebox(title="Result", message="To produce the items you need:\nNothing", option_1="Close")
                 return
             msg = "To produce the items you need:\n"
             for material, requirement in sorted(production_report.items(), key=lambda x: x[0].complexity_rating):
-                msg += material.name + ": " + "{:.2f}".format(requirement) + "\n"
+                msg += material.names[0] + ": " + "{:.2f}".format(requirement) + "\n"
             choice = CTkMessagebox(title="Result", message=msg, option_1="Close", option_2="Copy Results")
             if choice.get() == "Copy Results":
                 requested_items = "The requested items were:\n"
                 for material, requirement in sorted(data.items(),
-                                                    key=lambda x: Material.material_dict[x[0]].complexity_rating):
+                                                    key=lambda x: Material.name_to_item_dict[x].complexity_rating):
                     requested_items += material + ": " + "{:.2f}".format(requirement) + "\n"
                     root.clipboard_clear()
                     root.clipboard_append(requested_items + "\n" + msg)
                     root.update()
 
+        def item_selected(name):
+            current_item, recipe_index = Material.name_to_item_dict[name]
+            current_item.recipe = recipe_index
+
         root = tk.CTk()
         root.title("Material Calculator")
         # Create an empty list to store pairs of Entry and Label widgets
         entry_label_pairs = []
+        rows = ((len(Material.material_list) % 5)*2)+1
+        columns = (len(Material.material_list) // 5)
+
 
         # Add initial entries
-        for i, (name, item) in enumerate(Material.material_dict.items()):
-            label = tk.CTkLabel(root, text=name)
-            entry = tk.CTkEntry(root)
-            label.grid(column=i//5, row=(i % 5)*2)
+        for i, item in enumerate(Material.material_list):
+            width = 160
+            var = tk.StringVar()
+            menu = tk.CTkOptionMenu(root, variable=var, values=item.names, dropdown_hover_color="gray30",
+                                    fg_color="DodgerBlue4", button_color="gray30", button_hover_color="gray60", width=width,
+                                  command=item_selected)
+            menu.set(item.names[item.recipe])
+            entry = tk.CTkEntry(root, width=width)
+            menu.grid(column=i//5, row=(i % 5)*2)
             entry.grid(column=i//5, row=((i % 5)*2)+1)
-            entry_label_pairs.append((entry, label))
+            entry_label_pairs.append((entry, menu))
+
 
         # Add button to send data to the backend
         send_button = tk.CTkButton(root, text="Calculate Materials", command=calculate_materials)
         send_button.grid(row=len(entry_label_pairs) + 1, column=0, columnspan=2)
+
+
 
         root.mainloop()
--- Material.py
+++ Material.py
@@ -9,105 +9,115 @@ def combineDict(dict1, dict2):
     for key, value in dict2.items():
         addToDict(dict1, key, value)
 
+def receipeToText(recipe):
+    text = ""
+    for material, amount in recipe.items():
+        text += material + "  X " + str(amount) + "\n"
+    return text
 
 class Material:
     complexity = 0
 
-    def __init__(self, name, requirements=None):
+    def __init__(self, names, requirements=None, *, default_recipe=0):
         if requirements is None:
-            requirements = {}
-        self.name = name
+            requirements = [{}]
+        self.names = names
         self.requirements = requirements
+        self.recipe = default_recipe
         self.complexity_rating = Material.complexity
         Material.complexity += 1
 
     def produce(self, amount):
         materialDict = {}
-        for material, requirement in self.requirements.items():
+        for material, requirement in self.requirements[self.recipe].items():
             addToDict(materialDict, material, requirement * amount)
             combineDict(materialDict, material.produce(requirement * amount))
         return materialDict
 
     def printProduction(self, amount):
         production = self.produce(amount)
-        print("To produce ", amount, " of ", self.name, " you need:")
+        print("To produce ", amount, " of ", self.names[self.recipe], " you need:")
         for material, requirement in production.items():
             print(material.name, ": ", "{:.2f}".format(requirement))
 
 
-ironOre = Material("Iron Ore")
-copperOre = Material("Copper Ore")
-limestone = Material("Limestone")
-cateriumOre = Material("Caterium Ore")
-coal = Material("Coal")
-oil = Material("Oil")
-ironIngot = Material("Iron Ingot", {ironOre: 0.4, copperOre: 0.4})
-copperIngot = Material("Copper Ingot", {copperOre: 1})
-cateriumIngot = Material("Caterium Ingot", {cateriumOre: 3})
-concrete = Material("Concrete", {limestone: 3})
-ironPlate = Material("Iron Plate", {ironIngot: 1.5})
-steelIngot = Material("Steel Ingot", {ironIngot: 0.666, coal: 0.666})
-ironRod = Material("Iron Rod", {steelIngot: 0.25})
-wire = Material("Wire", {cateriumIngot: 0.033, copperIngot: 0.133})
-quickwire = Material("Quickwire", {cateriumIngot: 0.2})
-steelBeam = Material("Steel Beam", {steelIngot: 4})
-screw = Material("Screw", {steelBeam: 0.019})
-reinforcedIronPlate = Material("Reinforced Iron Plate", {ironPlate: 6, screw: 16.666})
-copperSheet = Material("Copper Sheet", {copperIngot: 2})
-plastic = Material("Plastic", {oil: 1.5})
-rubber = Material("Rubber", {oil: 1.5})
-cable = Material("Cable", {wire: 0.45, rubber: 0.3})
-steelPipe = Material("Steel Pipe", {steelIngot: 1.5})
-rotor = Material("Rotor", {ironRod: 5, screw: 25})
-encasedIndustrialBeam = Material("Encased Industrial Beam", {steelBeam: 4, concrete: 5})
-stator = Material("Stator", {steelPipe: 2, quickwire: 7.5})
-modularFrame = Material("Modular Frame", {reinforcedIronPlate: 1.5, screw: 28})
-smartPlating = Material("Smart Plating", {reinforcedIronPlate: 1, stator: 1})
-motor = Material("Motor", {rotor: 2, stator: 2})
-automatedWiring = Material("Automated Wiring", {stator: 1, cable: 20})
-circuitBoard = Material("Circuit Board", {copperSheet: 2, plastic: 4})
-computer = Material("Computer", {circuitBoard: 10, cable: 9, plastic: 18, screw: 52})
-heavyModularFrame = Material("Heavy Modular Frame",
-                             {modularFrame: 5, steelPipe: 15, encasedIndustrialBeam: 5, screw: 100})
-versatileFramework = Material("Versatile Framework", {modularFrame: 0.5, steelBeam: 6})
-modularEngine = Material("Modular Engine", {motor: 2, rubber: 15, smartPlating: 2})
-adaptiveControlUnit = Material("Adaptive Control Unit",
-                               {automatedWiring: 7.5, circuitBoard: 5, heavyModularFrame: 1, computer: 1})
-material_dict = {
-    "Iron Ore": ironOre,
-    "Copper Ore": copperOre,
-    "Limestone": limestone,
-    "Caterium Ore": cateriumOre,
-    "Coal": coal,
-    "Oil": oil,
-    "Iron Ingot": ironIngot,
-    "Copper Ingot": copperIngot,
-    "Caterium Ingot": cateriumIngot,
-    "Concrete": concrete,
-    "Iron Plate": ironPlate,
-    "Steel Ingot": steelIngot,
-    "Iron Rod": ironRod,
-    "Wire": wire,
-    "Quickwire": quickwire,
-    "Steel Beam": steelBeam,
-    "Screw": screw,
-    "Reinforced Iron Plate": reinforcedIronPlate,
-    "Copper Sheet": copperSheet,
-    "Plastic": plastic,
-    "Rubber": rubber,
-    "Cable": cable,
-    "Steel Pipe": steelPipe,
-    "Rotor": rotor,
-    "Encased Industrial Beam": encasedIndustrialBeam,
-    "Stator": stator,
-    "Modular Frame": modularFrame,
-    "Smart Plating": smartPlating,
-    "Motor": motor,
-    "Automated Wiring": automatedWiring,
-    "Circuit Board": circuitBoard,
-    "Computer": computer,
-    "Heavy Modular Frame": heavyModularFrame,
-    "Versatile Framework": versatileFramework,
-    "Modular Engine": modularEngine,
-    "Adaptive Control Unit": adaptiveControlUnit
-}
+ironOre = Material(["Iron Ore"])
+copperOre = Material(["Copper Ore"])
+limestone = Material(["Limestone"])
+cateriumOre = Material(["Caterium Ore"])
+coal = Material(["Coal"])
+oil = Material(["Oil"])
+ironIngot = Material(["Iron Ingot", "Iron Alloy ingot"], [{ironOre: 1}, {ironOre: 0.4, copperOre: 0.4}], default_recipe=1)
+copperIngot = Material(["Copper Ingot"], [{copperOre: 1}])
+cateriumIngot = Material(["Caterium Ingot"], [{cateriumOre: 3}])
+concrete = Material(["Concrete"], [{limestone: 3}])
+ironPlate = Material(["Iron Plate"], [{ironIngot: 1.5}])
+steelIngot = Material(["Steel Ingot"], [{ironIngot: 0.666, coal: 0.666}])
+ironRod = Material(["Iron Rod", "Steel Rod"], [{ironOre: 1}, {steelIngot: 0.25}], default_recipe=1)
+wire = Material(["Wire", "Fused Wire"], [{copperIngot: 0.5},{cateriumIngot: 0.033, copperIngot: 0.133}], default_recipe=1)
+quickwire = Material(["Quickwire"], [{cateriumIngot: 0.2}])
+steelBeam = Material(["Steel Beam"], [{steelIngot: 4}])
+screw = Material(["Screw", "Cast Screw", "Steel Screw"], [{ironRod: 0.25}, {ironIngot: 0.25}, {steelBeam: 0.019}], default_recipe=2)
+reinforcedIronPlate = Material(["Reinforced Iron Plate", "Stitched Iron Plate", "Bolted Iron Plate"],
+                               [{ironPlate: 6, screw: 12}, {ironPlate: 3.333, wire: 6.666 }, {ironPlate: 6, screw: 16.666}], default_recipe=2)
+copperSheet = Material(["Copper Sheet"], [{copperIngot: 2}])
+plastic = Material(["Plastic"], [{oil: 1.5}])
+rubber = Material(["Rubber"], [{oil: 1.5}])
+cable = Material(["Cable", "Insulated Cable"], [{wire: 2}, {wire: 0.45, rubber: 0.3}], default_recipe=1)
+steelPipe = Material(["Steel Pipe"], [{steelIngot: 1.5}])
+rotor = Material(["Rotor"], [{ironRod: 5, screw: 25}])
+encasedIndustrialBeam = Material(["Encased Industrial Beam"], [{steelBeam: 4, concrete: 5}])
+stator = Material(["Stator", "Quickwire Stator"], [{steelPipe: 3, wire: 8}, {steelPipe: 2, quickwire: 7.5}], default_recipe=1)
+modularFrame = Material(["Modular Frame", "Bolted Frame"], [{reinforcedIronPlate: 1.5, ironRod: 6}, {reinforcedIronPlate: 1.5, screw: 28}], default_recipe=1)
+smartPlating = Material(["Smart Plating"], [{reinforcedIronPlate: 1, stator: 1}])
+motor = Material(["Motor"], [{rotor: 2, stator: 2}])
+automatedWiring = Material(["Automated Wiring"], [{stator: 1, cable: 20}])
+circuitBoard = Material(["Circuit Board"], [{copperSheet: 2, plastic: 4}])
+computer = Material(["Computer"], [{circuitBoard: 10, cable: 9, plastic: 18, screw: 52}])
+heavyModularFrame = Material(["Heavy Modular Frame"], [{modularFrame: 5, steelPipe: 15, encasedIndustrialBeam: 5, screw: 100}])
+versatileFramework = Material(["Versatile Framework"], [{modularFrame: 0.5, steelBeam: 6}])
+modularEngine = Material(["Modular Engine"], [{motor: 2, rubber: 15, smartPlating: 2}])
+adaptiveControlUnit = Material(["Adaptive Control Unit"], [{automatedWiring: 7.5, circuitBoard: 5, heavyModularFrame: 1, computer: 1}])
+material_list = [
+    ironOre,
+    copperOre,
+    limestone,
+    cateriumOre,
+    coal,
+    oil,
+    ironIngot,
+    copperIngot,
+    cateriumIngot,
+    concrete,
+    ironPlate,
+    steelIngot,
+    ironRod,
+    wire,
+    quickwire,
+    steelBeam,
+    screw,
+    reinforcedIronPlate,
+    copperSheet,
+    plastic,
+    rubber,
+    cable,
+    steelPipe,
+    rotor,
+    encasedIndustrialBeam,
+    stator,
+    modularFrame,
+    smartPlating,
+    motor,
+    automatedWiring,
+    circuitBoard,
+    computer,
+    heavyModularFrame,
+    versatileFramework,
+    modularEngine,
+    adaptiveControlUnit
+]
+name_to_item_dict = {}
+
+for item in material_list:
+    for i, name in enumerate(item.names):
+        name_to_item_dict[name] = (item, i)
